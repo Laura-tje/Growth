51ebcb656b57be504e41e3affd5584f3d9c87736
Laura Delissen <lauradelissen@hotmail.com>
2026-05-28 10:25:34 +0200

    Added OptionsMenu but not fully working

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,24 +7,31 @@ public class UIManager : MonoBehaviour
         StartScreen,
         PlayMode,
         PauseMode,
+        OptionsMode,
         EndScreen,
     }
 
     private Menu currentState;
     
-    [SerializeField] private GameObject PauseMenu;
-    [SerializeField] private GameObject PauseButton;
-    [SerializeField] private GameObject BeginScreen;
-    [SerializeField] private GameObject EndScreen;
+    private GameObject PauseMenu;
+    private GameObject OptionsMenu;
+    private GameObject BeginScreen;
+    private GameObject EndScreen;
+    
+    private GameObject PauseButton;
+
     
     
     
     void Start()
     {
         PauseMenu =  GameObject.Find("PauseMenu");
-        PauseButton = GameObject.Find("PauseButton");
         BeginScreen = GameObject.Find("BeginScreen");
         EndScreen = GameObject.Find("EndScreen");
+        OptionsMenu = GameObject.Find("OptionsMenu");
+        
+        PauseButton = GameObject.Find("PauseButton");
+
         
         currentState = Menu.StartScreen;
     }
@@ -36,27 +43,40 @@ public class UIManager : MonoBehaviour
         {
             case Menu.StartScreen:
                 BeginScreen.SetActive(true);
-                PauseButton.SetActive(false);
                 PauseMenu.SetActive(false);
                 EndScreen.SetActive(false);
+                
+                PauseButton.SetActive(false);
                 break;
             case Menu.PlayMode:
                 BeginScreen.SetActive(false);
-                PauseButton.SetActive(true);
                 PauseMenu.SetActive(false);
                 EndScreen.SetActive(false);
+                
+                PauseButton.SetActive(true);
                 break;
             case Menu.PauseMode:
                 BeginScreen.SetActive(false);
-                PauseButton.SetActive(false);
                 PauseMenu.SetActive(true);
+                OptionsMenu.SetActive(false);
                 EndScreen.SetActive(false);
+                
+                PauseButton.SetActive(false);
+                break;
+            case Menu.OptionsMode:
+                BeginScreen.SetActive(false);
+                PauseMenu.SetActive(false);
+                OptionsMenu.SetActive(true);
+                EndScreen.SetActive(false);
+                
+                PauseButton.SetActive(false);
                 break;
             case Menu.EndScreen:
                 BeginScreen.SetActive(false);
-                PauseButton.SetActive(false);
                 PauseMenu.SetActive(false);
                 EndScreen.SetActive(true);
+                
+                PauseButton.SetActive(false);
                 break;
         }
     }
