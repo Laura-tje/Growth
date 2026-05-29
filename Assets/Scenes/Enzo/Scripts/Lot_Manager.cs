f1784e1a33122bdd181a498967823a4fd94b3e39
NqpTn0rm4l <barrientosenzoamenemiguel@gmail.com>
2026-05-29 12:28:24 +0200

    Growth script

--- Assets/Scenes/Enzo/Scripts/Lot_Manager.cs
+++ Assets/Scenes/Enzo/Scripts/Lot_Manager.cs
@@ -9,14 +9,18 @@ public class Lot_Manager : MonoBehaviour
 
     [SerializeField] private bool _Plant_Still_Growing;
     [SerializeField] private bool _Plant_Done_Growing;
-
-    private int _Current_Amount_Mats;
+    [SerializeField] private bool _Transfering;
+ 
+    [SerializeField] private int _Current_Amount_Mats;
     private int _Max_Amount_Mats_Grow;
 
     private void Start()
     {
 
         _Check_Plant();
+        _Plant_Still_Growing = true;
+        _Plant_Done_Growing = false;
+        _Transfering = false;
 
     }
 
@@ -36,7 +40,9 @@ public class Lot_Manager : MonoBehaviour
             if ( _Plant.transform.GetChild(0).gameObject.name == "Plant_Type_1")
             {
 
-                
+                _Plant_Growth_Plan_1();
+
+                Debug.Log("Plant type 1 growth plan assigned to lot");
 
             }
 
@@ -44,34 +50,105 @@ public class Lot_Manager : MonoBehaviour
 
     }
 
-    private void _Plant_Growth_Plan()
+    private void _Plant_Growth_Plan_1()
     {
 
-        int Mats_Needed = _Max_Amount_Mats_Grow;
+        int Mats_Needed = 50;
 
-
+        _Max_Amount_Mats_Grow = Mats_Needed;
 
     }
 
+     
     private void OnTriggerStay(Collider other)
     {
 
-        if (other.gameObject == _Player)
+        if ( other.gameObject == _Player && _Plant_Still_Growing && !_Transfering)
         {
 
-            Player_Growth_Corrosponding_Script player_Script = _Player.GetComponent<Player_Growth_Corrosponding_Script>();
+            Debug.Log("Player in lot");
 
-            while (player_Script._Player_Mats_Owned > 0 && _Current_Amount_Mats < _Max_Amount_Mats_Grow)
+            StartCoroutine(_Transfer_Mats(other));
+
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if ( other.gameObject == _Player && _Plant_Still_Growing && _Transfering)
+        {
+
+            StopCoroutine(_Transfer_Mats(other));
+
+            _Transfering = false;
+
+        }
+
+    }
+
+    private IEnumerator _Transfer_Mats(Collider other)
+    {
+
+        Debug.Log("Attempting to transfer mats");
+
+        _Transfering = true;
+
+        Player_Growth_Corrosponding_Script player_Script = _Player.GetComponent<Player_Growth_Corrosponding_Script>();
+
+        if ( other != null && player_Script._Player_Mats_Owned > 0)
+        {
+
+            if ( _Current_Amount_Mats >= _Max_Amount_Mats_Grow)
             {
 
-                player_Script._Player_Mats_Owned -= 1;
+                yield return null;
 
-                _Current_Amount_Mats += 1;
+                _Transfering = false;
+
+                Debug.Log("Transfering mats stopped");
+
+            }
+
+            else if ( _Current_Amount_Mats <= _Max_Amount_Mats_Grow)
+            {
+
+                while ( _Current_Amount_Mats <= _Max_Amount_Mats_Grow)
+                {
+
+                    player_Script._Player_Mats_Owned --;
+
+                    _Current_Amount_Mats ++;
+
+                    yield return new WaitForSeconds(0.1f);  
+
+                    Debug.Log("Transferring mats");
+
+                    _Transfering = true;
+
+                }
 
             }
 
         }
 
     }
-    
+
+    private void Update()
+    {
+
+        if ( _Current_Amount_Mats >= _Max_Amount_Mats_Grow)
+        {
+
+            _Plant_Still_Growing = false;
+
+            _Plant_Done_Growing = true;
+
+            Debug.Log("Plant done growing");
+
+        }
+
+    }
+
 }
--- Assets/Scenes/Enzo/Scripts/Player_Growth_Corrosponding_Script.cs
+++ Assets/Scenes/Enzo/Scripts/Player_Growth_Corrosponding_Script.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Player_Growth_Corrosponding_Script : MonoBehaviour
 {
 
-    [SerializeField] private int _Player_Mats_Owned;
+    public int _Player_Mats_Owned;
 
     public void Start()
     {
