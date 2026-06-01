441ad29962461e9f82c81fce5308259c6667b3e9
Tom(Lepsko) <tommiemichaux@gmail.com>
2026-06-01 14:15:37 +0200

    Fix: Fix Inventory FINAL

--- Assets/Devs/Tom/Scripts/Scriptable Objetcs/ItemForGrowth.cs
+++ Assets/Devs/Tom/Scripts/Scriptable Objetcs/ItemForGrowth.cs
@@ -9,10 +9,23 @@ public class ItemForGrowth : ScriptableObject
 
     public int soilNeeded;
 
+    public int allItemsNeeded;
+
     public int currentAmountOfSeeds;
 
     public int currentAmountOfWater;
 
     public int currentAmountOfSoil;
 
+    public int currentAmountOfAllItems;
+
+    public void MaxAmountOfItemsNeeded()
+    {
+        allItemsNeeded = seedsNeeded + waterNeeded + soilNeeded;
+    }
+
+    public void CurrentAmountOfItems()
+    {
+        currentAmountOfAllItems = currentAmountOfSeeds + currentAmountOfWater + currentAmountOfSoil;
+    }
 }
