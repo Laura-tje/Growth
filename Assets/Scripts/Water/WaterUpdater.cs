ef29901409af78c1b50475d59a61ed4c48a623cd
Laura Delissen <lauradelissen@hotmail.com>
2026-06-10 11:10:36 +0300

    added requirements to waterwell

--- Assets/Scripts/Water/WaterUpdater.cs
+++ Assets/Scripts/Water/WaterUpdater.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
+using TMPro;
 
 public class WaterUpdater : MonoBehaviour
 {
     [SerializeField] WaterWell Well;
 
-    [SerializeField] private float neededAmount;
+    public float neededAmount;
     [SerializeField] private float increaseAmount;
 
     private Inventory inventoryScript;
     private GameObject player;
+
+    //public float ItemsNeeded;
+    [SerializeField] TextMeshProUGUI ItemsNeededText;
+    [SerializeField] private float[] upgradeRequirements = { 5f, 10f, 20f };
+    private int currentUpgradeIndex = 0;
     
     void Start()
     {
@@ -30,34 +36,66 @@ public class WaterUpdater : MonoBehaviour
         
         Well.Inventory = inventoryScript;
         
+        neededAmount = upgradeRequirements[currentUpgradeIndex];
+        ItemsNeededText.text = neededAmount.ToString();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Player")
         {
+            float amountOfCrops = 0;
             Debug.Log("Player stubbed his toe against the water updater");
+            Debug.Log("Give Crop tag to crop prefab");//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            for (int i = 0; i < inventoryScript.InventoryItems.Count; i++)
+            {
+                if (inventoryScript.InventoryItems[i].CompareTag("Crop"))
+                {
+                    amountOfCrops += 1;
+                }
+            }
 
-            if ( /*inventoryScript.amountofcrops >= neededAmount*/ true) //FIX THISSSSS WHEN TOM MAKES IT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            if ( amountOfCrops >= neededAmount)
             {
+                neededAmount = 0;
                 Well.UpdateWell();
-                //inventory -= neededamount
+                //inventory -= neededamount; take them from inventory!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                amountOfCrops = 0;
+                ItemsNeededText.text = neededAmount.ToString();
                 Debug.Log("The player successfully updated the water well to convenience himself. How selfish...");
-            } else if ( /*inventoryScript.amountofcrops < neededAmount*/ false) //FIX THISSSS AS WELLLLLLLLLLL (WATERWELL)!!!!!!!!!!!!!!!!!!!!!!
+            } else if ( amountOfCrops < neededAmount)
             {
-                /*let amount = inventoryScript.amountofcrops
-                inventory -= amount
-                neededamount -= amount*/
+                neededAmount -= amountOfCrops;
+                ItemsNeededText.text = neededAmount.ToString();
+                //neededamount -= amount; take them from inventory!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                amountOfCrops = 0;
                 Debug.Log("The player has miscounted their crops and has to go get more. Until then the crops will be safely putt away.");
 
             }
             
+        }
+    }
+
+    public void UpdateNeededAmount()
+    {
+        currentUpgradeIndex++;
+
+        if (currentUpgradeIndex < upgradeRequirements.Length)
+        {
+            neededAmount = upgradeRequirements[currentUpgradeIndex];
+            ItemsNeededText.text = neededAmount.ToString();
+        }
+        else
+        {
+            ItemsNeededText.text = "MAX";
         }
     }
 }
