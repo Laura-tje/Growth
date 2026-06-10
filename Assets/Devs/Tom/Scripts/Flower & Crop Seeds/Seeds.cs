23ecadc2ebdf2f85eb9c70ccf71defe3f7aa24d5
Tom(Lepsko) <tommiemichaux@gmail.com>
2026-06-10 09:34:11 +0300

    Fix infinite coroutine loop

--- Assets/Devs/Tom/Scripts/Flower & Crop Seeds/Seeds.cs
+++ Assets/Devs/Tom/Scripts/Flower & Crop Seeds/Seeds.cs
@@ -1,9 +1,13 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Seeds : MonoBehaviour
 { 
+    [SerializeField] public Lot_Manager.TypeFlowers flowerSeed;
 
-    [SerializeField] public Lot_Manager.TypeFlowers flowerSeed; 
+    [SerializeField] private int hitLives;
+
 
     private enum CropSeeds
     {
@@ -30,7 +34,29 @@ public class Seeds : MonoBehaviour
     {
         if(other.gameObject.tag == "Player")
         {
-            other.GetComponentInChildren<Inventory>().AddObjectToInventory(gameObject);
+            //other.GetComponentInChildren<Inventory>().AddObjectToInventory(gameObject);
+            StartCoroutine(WhackSeeds());
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //other.GetComponentInChildren<Inventory>().AddObjectToInventory(gameObject);
+            StopCoroutine(WhackSeeds());
+        }
+    }
+
+    private IEnumerator WhackSeeds()
+    {
+        while(hitLives > 0)
+        {
+            hitLives -= 1;
+            yield return new WaitForSeconds(1f);
+        }
+        Debug.Log("Dies");
+       
+        yield return null;
     }
 }
