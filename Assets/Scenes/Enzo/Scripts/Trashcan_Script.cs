db14d25217616f986987beab7661274dede45d19
Neutron <barrientosenzoamenemiguel@gmail.com>
2026-06-16 16:57:11 +0300

    working trash

---
+++ Assets/Scenes/Enzo/Scripts/Trashcan_Script.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+
+public class Trashcan_Script : MonoBehaviour
+{
+    private GameObject player;
+    private Inventory inventory;
+    private bool throwing;
+    private void Start()
+    {
+        throwing = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inventory = other.GetComponentInChildren<Inventory>();
+            throwing = true;
+            StartCoroutine(Throw_Inventory());
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            throwing = false;
+        }
+    }
+    private IEnumerator Throw_Inventory()
+    {
+        if (throwing == true && inventory.InventoryItems.Count > 0)
+        {
+            yield return new WaitForSeconds(1.5f);
+            foreach (GameObject item in inventory.InventoryItems)
+            {
+                Destroy(item);  
+            }
+        }
+        inventory.InventoryItems.Clear();
+    }
+}
