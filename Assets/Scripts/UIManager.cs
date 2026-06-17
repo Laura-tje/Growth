4eaa7e66fbb8cd75988a731386278913afa4e622
Laura Delissen <lauradelissen@hotmail.com>
2026-06-17 10:54:23 +0300

    ui sound toegevoegd

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -89,31 +89,35 @@ public class UIManager : MonoBehaviour
     
     public void PauseButtonClicked()
     {
+        Sound_Manage_III.Instance._Play_Sound(1);
         currentState = Menu.PauseMode;
-        Sound_Manage_III.Instance._Play_Sound(0);
         UpdateUI();
     }
 
     public void PlayButtonClicked()
     {
+        Sound_Manage_III.Instance._Play_Sound(1);
         currentState = Menu.PlayMode;
         UpdateUI();
     }
 
     public void RestartButtonClicked()
     {
+        Sound_Manage_III.Instance._Play_Sound(1);
         currentState = Menu.StartScreen;
         UpdateUI();
     }
 
     public void OptionsButtonClicked()
     {
+        Sound_Manage_III.Instance._Play_Sound(1);
         currentState = Menu.OptionsMode;
         UpdateUI();
     }
 
     public void QuitButtonClicked()
     {
+        Sound_Manage_III.Instance._Play_Sound(1);
         Debug.Log("Game Afgesloten");
         Application.Quit();
     }
